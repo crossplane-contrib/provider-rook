b7f1251893599ae240994f7632bd4da8e756252c
hasheddan <georgedanielmangum@gmail.com>
2019-10-11 15:34:06 -0500

    yugabyte: implement tests for claim reconciler
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- pkg/controller/database/claim_test.go
+++ pkg/controller/database/claim_test.go
@@ -17,9 +17,114 @@ limitations under the License.
 package database
 
 import (
+	"context"
+	"testing"
+
+	runtimev1alpha1 "github.com/crossplaneio/crossplane-runtime/apis/core/v1alpha1"
 	"github.com/crossplaneio/crossplane-runtime/pkg/resource"
+	"github.com/crossplaneio/crossplane-runtime/pkg/test"
+	databasev1alpha1 "github.com/crossplaneio/crossplane/apis/database/v1alpha1"
+	"github.com/crossplaneio/stack-rook/apis/database/v1alpha1"
+	"github.com/google/go-cmp/cmp"
+	rook "github.com/rook/rook/pkg/apis/rook.io/v1alpha2"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 var (
 	_ resource.ManagedConfigurator = resource.ManagedConfiguratorFn(ConfigureYugabyteCluster)
 )
+
+func TestConfigureYugabyteCluster(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		cm  resource.Claim
+		cs  resource.NonPortableClass
+		mg  resource.Managed
+	}
+
+	type want struct {
+		mg  resource.Managed
+		err error
+	}
+
+	claimUID := types.UID("definitely-a-uuid")
+	providerName := "coolprovider"
+
+	server := v1alpha1.ServerSpec{
+		Replicas: 3,
+		Network: v1alpha1.NetworkSpec{
+			Ports: []v1alpha1.PortSpec{
+				v1alpha1.PortSpec{
+					Name: "cool-port",
+					Port: 7000,
+				},
+			},
+		},
+		VolumeClaimTemplate: corev1.PersistentVolumeClaim{},
+	}
+
+	params := v1alpha1.YugabyteClusterParameters{
+		Name:        "cool-yugabyte",
+		Namespace:   "cool-yugabyte-ns",
+		Annotations: rook.Annotations(map[string]string{"label": "value"}),
+		Master:      server,
+		TServer:     server,
+	}
+
+	cases := map[string]struct {
+		args args
+		want want
+	}{
+		"Successful": {
+			args: args{
+				cm: &databasev1alpha1.PostgreSQLInstance{
+					ObjectMeta: metav1.ObjectMeta{UID: claimUID},
+					Spec: databasev1alpha1.PostgreSQLInstanceSpec{
+						ResourceClaimSpec: runtimev1alpha1.ResourceClaimSpec{
+							PortableClassReference:           &corev1.LocalObjectReference{},
+							WriteConnectionSecretToReference: corev1.LocalObjectReference{},
+							ResourceReference:                nil,
+						},
+					},
+				},
+				cs: &v1alpha1.YugabyteClusterClass{
+					SpecTemplate: v1alpha1.YugabyteClusterClassSpecTemplate{
+						NonPortableClassSpecTemplate: runtimev1alpha1.NonPortableClassSpecTemplate{
+							ProviderReference: &corev1.ObjectReference{Name: providerName},
+							ReclaimPolicy:     runtimev1alpha1.ReclaimDelete,
+						},
+						YugabyteClusterParameters: params,
+					},
+				},
+				mg: &v1alpha1.YugabyteCluster{},
+			},
+			want: want{
+				mg: &v1alpha1.YugabyteCluster{
+					Spec: v1alpha1.YugabyteClusterSpec{
+						ResourceSpec: runtimev1alpha1.ResourceSpec{
+							ProviderReference:                &corev1.ObjectReference{Name: providerName},
+							ReclaimPolicy:                    runtimev1alpha1.ReclaimDelete,
+							WriteConnectionSecretToReference: corev1.LocalObjectReference{Name: string(claimUID)},
+						},
+						YugabyteClusterParameters: params,
+					},
+				},
+				err: nil,
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			err := ConfigureYugabyteCluster(tc.args.ctx, tc.args.cm, tc.args.cs, tc.args.mg)
+			if diff := cmp.Diff(tc.want.err, err, test.EquateErrors()); diff != "" {
+				t.Errorf("ConfigureYugabyteCluster(...): -want error, +got error:\n%s", diff)
+			}
+			if diff := cmp.Diff(tc.want.mg, tc.args.mg, test.EquateConditions()); diff != "" {
+				t.Errorf("ConfigureYugabyteCluster(...) Managed: -want, +got:\n%s", diff)
+			}
+		})
+	}
+}
