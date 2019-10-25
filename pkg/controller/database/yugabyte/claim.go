1a5c2192a935253bb0f6010100ea33981c9d81ed
Nic Cope <negz@rk0n.org>
2019-10-24 21:57:46 -0700

    Update crossplane-runtime to fix static provisioning
    See https://github.com/crossplaneio/crossplane-runtime/pull/55
    
    Signed-off-by: Nic Cope <negz@rk0n.org>

--- pkg/controller/database/yugabyte/claim.go
+++ pkg/controller/database/yugabyte/claim.go
@@ -106,7 +106,7 @@ func (c *ClaimController) SetupWithManager(mgr ctrl.Manager) error {
 		resource.ClaimKind(databasev1alpha1.PostgreSQLInstanceGroupVersionKind),
 		resource.ClassKind(v1alpha1.YugabyteClusterClassGroupVersionKind),
 		resource.ManagedKind(v1alpha1.YugabyteClusterGroupVersionKind),
-		resource.WithManagedBinder(resource.NewAPIManagedStatusBinder(mgr.GetClient())),
+		resource.WithManagedBinder(resource.NewAPIManagedStatusBinder(mgr.GetClient(), mgr.GetScheme())),
 		resource.WithManagedFinalizer(resource.NewAPIManagedStatusUnbinder(mgr.GetClient())),
 		resource.WithManagedConfigurators(
 			resource.ManagedConfiguratorFn(ConfigureYugabyteCluster),
